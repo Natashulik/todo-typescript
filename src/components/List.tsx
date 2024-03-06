481e07f237a9ef85c067c9d65cfcaa28603aad1b
Natashulik <nata17v@mail.ru>
2024-03-06 14:31:50 +0300

    feat: implement drag and drop

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useEffect } from "react";
+import { ChangeEvent, useEffect, useRef } from "react";
 import { useAppSelector, useAppDispatch } from "../hook";
 import {
   changeTaskTitle,
@@ -8,8 +8,9 @@ import {
   setTasks,
 } from "../redux/listSlice";
 import { loadTasks, saveTasks } from "../utils/localStorage";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-const Task = (): JSX.Element => {
+const List = (): JSX.Element => {
   const selectedButton = useAppSelector(
     (state) => state.selection.selectedButton
   );
@@ -51,45 +52,84 @@ const Task = (): JSX.Element => {
     saveTasks(tasks);
   }, [tasks]);
 
+  const handleDragEnd = (result: any) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const tasksCopy = Array.from(tasks);
+    const [reorderedTask] = tasksCopy.splice(result.source.index, 1);
+    tasksCopy.splice(result.destination.index, 0, reorderedTask);
+
+    dispatch(setTasks(tasksCopy));
+  };
+
   return (
-    <>
-      {filtredTasks.map((item) => (
-        <div className="task-wrap" key={item.id}>
-          <input
-            type="checkbox"
-            className="checkbox"
-            checked={item.completed ? true : false}
-            onChange={() => todoCompleted(item.id, item.completed)}
-          />
-          {item.editMode ? (
-            <input
-              value={item.title}
-              onChange={(event) => changeInput(item.id, event)}
-              className={
-                item.completed ? "input-task-title decor" : "input-task-title"
-              }
-            />
-          ) : (
-            <p className={item.completed ? "task-title decor" : "task-title"}>
-              {item.title}
-            </p>
-          )}
-          <button
-            className="button-edit"
-            onClick={() => toggle(item.id, item.editMode)}
-          >
-            {item.editMode ? "✔" : "✎"}{" "}
-          </button>
-          <button
-            className="button-edit"
-            onClick={() => handleDeleteTask(item.id)}
-          >
-            ✖
-          </button>
-        </div>
-      ))}
-    </>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <Droppable droppableId="task-list">
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef}>
+            {filtredTasks.map((item, index) => (
+              <Draggable
+                key={item.id.toString()}
+                draggableId={item.id.toString()}
+                index={index}
+              >
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <div className="task-wrap" key={item.id}>
+                      <input
+                        type="checkbox"
+                        className="checkbox"
+                        checked={item.completed ? true : false}
+                        onChange={() => todoCompleted(item.id, item.completed)}
+                      />
+                      {item.editMode ? (
+                        <input
+                          value={item.title}
+                          onChange={(event) => changeInput(item.id, event)}
+                          className={
+                            item.completed
+                              ? "input-task-title decor"
+                              : "input-task-title"
+                          }
+                        />
+                      ) : (
+                        <p
+                          className={
+                            item.completed ? "task-title decor" : "task-title"
+                          }
+                        >
+                          {item.title}
+                        </p>
+                      )}
+                      <button
+                        className="button-edit"
+                        onClick={() => toggle(item.id, item.editMode)}
+                      >
+                        {item.editMode ? "✔" : "✎"}{" "}
+                      </button>
+                      <button
+                        className="button-edit"
+                        onClick={() => handleDeleteTask(item.id)}
+                      >
+                        ✖
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
-export default Task;
+export default List;
--- src/utils/defaultTasks.ts
+++ src/utils/defaultTasks.ts
@@ -2,19 +2,19 @@ import { Task } from "../types/taskTypes";
 
 export const defaultTasks: Task[] = [
   {
-    id: 1,
+    id: 155464,
     title: "shopping",
     completed: false,
     editMode: false,
   },
   {
-    id: 2,
+    id: 254658,
     title: "shopping",
     completed: false,
     editMode: false,
   },
   {
-    id: 3,
+    id: 355650,
     title: "more shopping",
     completed: false,
     editMode: false,
